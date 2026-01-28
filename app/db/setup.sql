88663d3d0b578d8fa793d237d5d6e49cb150b91d
Abdelsalam Mahari <abdelsalam.mahari@gmail.com>
2026-01-28 14:37:20 +0200

    initial push

---
+++ app/db/setup.sql
@@ -0,0 +1,40 @@
+-- This file contains the SQL schema, it drops all tables and recreates them
+
+DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS timesheets;
+
+-- To add a field to a table do
+-- CREATE TABLE table_name (
+--     id INTEGER PRIMARY KEY AUTOINCREMENT,
+--     nullable_field TEXT,
+--     non_nullable_field TEXT NOT NULL,
+--     numeric_field INTEGER,
+--     unique_field TEXT UNIQUE,
+--     unique_non_nullable_field TEXT NOT NULL UNIQUE,
+--     date_field DATE,
+--     datetime_field DATETIME
+-- );
+
+-- Create employees table
+CREATE TABLE employees (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    full_name TEXT NOT NULL,
+    email TEXT NOT NULL,
+    phone_number TEXT NOT NULL,
+    dob DATE NULL,
+    job_title TEXT NULL,
+    department TEXT NULL,
+    salary REAL NULL,
+    start_date DATETIME NULL,
+    end_date DATETIME NULL
+);
+
+-- Create timesheets table
+CREATE TABLE timesheets (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    summary TEXT NULL,
+    start_time DATETIME NOT NULL,
+    end_time DATETIME NOT NULL,
+    employee_id INTEGER NOT NULL,
+    FOREIGN KEY (employee_id) REFERENCES employees(id)
+);
